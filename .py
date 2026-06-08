24b393569147651eceb5258569a4719d66810b50
srikar-git-cpu <nagubandisrikar73@gmail.com>
2026-06-08 17:30:53 +0530

    app .py

--- .py
+++ .py
@@ -1,5 +1,52 @@
-name = 'srikar'
+course = "python's course for beginners"
+print(course[-2])
+
+course = "python's course for beginners"
+print(course[0:5])
+
+course = "python's course for beginners"
+print(course[3:])
+
+course = "python's course for beginners"
+print(course[:3])
+
+name ='srikar'
 print(name[1:-1])
 
 name ='srikar'
 print(name[1:3])
+
+first = 'srikar'
+last = 'nagubandi'
+message = first + ' [' + last + '] is a coder '
+msg = f'{first} [{last}] is a coder'
+print(message)
+
+first = 'srikar'
+last = 'nagubandi'
+message = first + ' [' + last + '] is a coder '
+msg = f'{first} [{last}] is a coder'
+print(message) 
+
+first = 'srikar'
+last = 'nagubandi'
+message = first + ' [' + last + '] is a coder '
+msg = f'{first} [{last}] is a coder'
+print(msg)
+
+course = 'srikar is learning python'
+print(len(course))
+
+course = 'python for beginners'
+print(course.upper())
+print(course.lower())
+
+course = 'python for beginners'
+print(course.find('y'))
+print(course.index('y'))
+
+print(course.replace ('beginners', 'absolute beginners'))
+
+print('python' in course)
+print('fur' in course)
+
